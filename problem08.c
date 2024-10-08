d57f98f707712878534b4a0ee118c0e67aca0040
Mursalin <mursalin2311@gmail.com>
2024-10-08 20:05:02 +0600

    solve the problem 8

---
+++ problem08.c
@@ -0,0 +1,29 @@
+/*
+    Pattern 2 start
+
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 6; i >= 1; i--)
+    {
+        // printf("%d \n", i);
+
+        for (int j = 1; j < i; j++)
+        {
+            printf("* ", i);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
