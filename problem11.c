589baa4b4fbcc3e7205c664678aa26a21d621c38
Mursalin <mursalin2311@gmail.com>
2024-10-08 22:54:30 +0600

    problem 11 done

---
+++ problem11.c
@@ -0,0 +1,28 @@
+/*
+    Pattern 2 start
+1 
+2 1 
+3 2 1
+4 3 2 1
+5 4 3 2 1
+6 5 4 3 2 1
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 6; i++)
+    {
+        // printf("%d \n", i);
+
+        for (int j = i; j >=1; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
