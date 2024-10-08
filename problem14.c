70377050a7b8631b83efb3ef0ca708ae10ea133d
Mursalin <mursalin2311@gmail.com>
2024-10-08 23:11:27 +0600

    problem 14 done

---
+++ problem14.c
@@ -0,0 +1,36 @@
+/*
+ Pattern 8 The piramid
+
+      1 
+     1 2
+    1 2 3
+   1 2 3 4
+  1 2 3 4 5
+ 1 2 3 4 5 6
+1 2 3 4 5 6 7
+
+*/
+#include <stdio.h>
+
+int main()
+
+{
+    int n = 7;
+    for (int i = 1; i <= n; i++)
+    {
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" "); // one space
+        }
+
+        // Print stars
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
